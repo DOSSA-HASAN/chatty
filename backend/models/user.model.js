c03300ff970f50b0b234cd028a7eb56372ec8e82
Hasan <hassandossa5@gmail.com>
2025-03-11 21:16:46 +0300

    added timestamps for user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-    email: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
-    username: {type: String, unique: true, required: true},
-    profilePic: {type: String, default: ""},
-})
+const UserSchema = new mongoose.Schema(
+    {
+        email: { type: String, unique: true, required: true },
+        password: { type: String, required: true },
+        username: { type: String, unique: true, required: true },
+        profilePic: { type: String, default: "" },
+    },
+    {
+        timestamps: true
+    }
+
+)
 
 export const User = mongoose.model('user', UserSchema)
