32aa23c2c02a6424239db16faa0cb411f87b2a9d
Hasan <hassandossa5@gmail.com>
2025-03-11 22:21:32 +0300

    middleware attachment for messaging routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv/config"
 import router from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
 import { connnectDB } from "./lib/dbConnection.js"
+import messageRoute from "./routes/message.route.js"
 
 const app = express()
 connnectDB()
@@ -11,6 +12,8 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use('/api/auth', router)
+app.use('/api/message', messageRoute)
 
 app.listen(PORT, () => { console.log(`server started on port : ${PORT}`)})
