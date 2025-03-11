5d3eab3bd8f62558210fbb836e09e8b8ed52ba40
Hasan <hassandossa5@gmail.com>
2025-03-11 21:17:14 +0300

    added get route for auth purposes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from "express"
-import { login, logout, register } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, register, updateProfile } from "../controllers/auth.controller.js";
+import { verifyUser } from "../lib/verify.js";
 
 const router = express.Router()
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
+router.put('/update-profile',  verifyUser, updateProfile)
+router.get('/check', verifyUser, checkAuth)
 
 export default router
