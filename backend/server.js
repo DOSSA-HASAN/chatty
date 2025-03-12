1a36a7de43e37f21dd494e5d56a58c2b0b17283c
Hasan <hassandossa5@gmail.com>
2025-03-12 22:54:48 +0300

    added cors

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import router from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
 import { connnectDB } from "./lib/dbConnection.js"
 import messageRoute from "./routes/message.route.js"
+import cors from 'cors'
 
 const app = express()
 connnectDB()
@@ -12,6 +13,10 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 
 app.use('/api/auth', router)
 app.use('/api/message', messageRoute)
