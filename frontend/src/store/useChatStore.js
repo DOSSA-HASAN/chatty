8603d214b179dc86af0369010e1c24a5109db99b
Hasan <hassandossa5@gmail.com>
2025-04-11 22:34:48 +0300

    added function to send messages

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand"
 import toast from "react-hot-toast"
 import { axiosInstance } from "../lib/axios"
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
     messages: [],
     users: [],
     selectedUser: null,
@@ -23,16 +23,28 @@ export const useChatStore = create((set) => ({
         }
     },
 
-    getMessages: async (userId) => {
+    getMessages: async (id) => {
         set({ isMessagesLoading: true })
         try {
-            const res = await axiosInstance.get(`/message/${userId}`)
+            const res = await axiosInstance.get(`/message/${id}`)
             set({ messages: res.data })
         } catch (error) {
             toast.error("An error occurred while retireving messages!")
             console.log(error.stack)
         } finally {
             set({ isMessagesLoading: false })
+        }
+    },
+
+    sendMessages: async (messageData) => {
+        const { selectedUser, messages } = get()
+        try {
+            const res = await axiosInstance.post(`/message/send/${selectedUser._id}`, messageData)
+            set({ messages: [...messages, res.data]})
+
+        } catch (error) {
+            console.log(error.message)
+            toast.error("Failed to send message")
         }
     },
 
