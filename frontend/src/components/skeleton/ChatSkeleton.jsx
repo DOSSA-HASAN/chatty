a7f7b2ecd4514e396bfd9431cd4e92a981bd52c2
Hasan <hassandossa5@gmail.com>
2025-04-12 22:04:05 +0300

    minor changes

--- frontend/src/components/skeleton/ChatSkeleton.jsx
+++ frontend/src/components/skeleton/ChatSkeleton.jsx
@@ -3,24 +3,21 @@ import { Send } from 'lucide-react'
 
 function ChatSkeleton() {
 
-    const messages = new Array(5).fill(" ")
+    const messages = new Array(7).fill(" ")
 
     return (
-        <section className='w-full flex flex-col justify-end items-end border-base-300'>
+        <section className='w-full flex p-3 flex-col border-base-300 maxh-[60%] overflow-hidden'>
             {
                 messages.map((mssg, idx) => (
-                    <div className={idx % 2 == 0 ? 'flex justify-start w-full' : 'flex justify-end w-full'}>
-                        <div className={idx % 2 == 0 ? 'flex flex-col justify-start items-start animate-pulse bg-base-300 text-primary-content w-1/2 h-20 rounded-md p-3 m-3' : 'flex flex-col justify-end items-end animate-pulse bg-base-300 text-secondary-content w-1/2 h-20 rounded-md p-3 m-3'}>
+                    <div key={idx} className={`flex flex mt-2 ${idx % 2 === 0 ? "justify-start items-start" : "justify-end items-end flex-row-reverse"}`}>
+                        <div className="rounded-full w-15 h-15 bg-base-300 mr-2 animate-pulse"></div>
+                        <div className={`flex w-full flex-col animate-pulse ${idx % 2 === 0 ? "justify-start items-start" : "justify-end items-end"}`}>
+                            <div className="w-[50%] bg-base-300 h-10 rounded-md mb-2 animate-pulse"></div>
+                            <div className="w-[20%] bg-base-300 h-5 rounded-md animate-pulse"></div>
                         </div>
                     </div>
                 ))
             }
-            <form className='w-full flex justify-between items-center'>
-                <input type="text" readOnly value="Type a message" className='w-[90%] h-13 p-5 border-2 border-secondary rounded-md focus:outline-none' />
-                <button className='btn btn-primary p-5 h-13 rounded-md hover:cursor-not-allowed' disabled>
-                    <Send className='send-icon' size={40} />
-                </button>
-            </form>
         </section>
     )
 }
